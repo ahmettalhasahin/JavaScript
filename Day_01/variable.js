a830edaba1be024ec880c8410e7b045a715fbf68
Ahmet talha Sahin <asahin@k1m41s05.42istanbul.com.tr>
2022-07-27 15:58:46 +0300

    javascript tests

---
+++ Day_01/variable.js
@@ -0,0 +1,30 @@
+let firstname = 'Ahmet'
+console.log("firstname :",firstname)
+
+let young = true
+console.log("young :",young)
+
+let ages = undefined
+console.log("ages :",ages)
+
+let salary = null
+console.log("salary :",salary)
+
+let a,
+	b,
+	c,
+	d
+
+let name = "Ahmet Talha"
+let surname = "Sahin"
+let maritalStatus = "single"
+let country = "Turkey"
+let age = 22
+console.log(name, surname+",", maritalStatus+",", country+",", age)
+
+let myAge = 22
+let yourAge = 20
+console.log("I am "+myAge+" years old.")
+console.log("You are "+yourAge+" years old.")
+console.log(`I am ${myAge} years old.`)
+console.log(`You are ${yourAge} years old.`)
---
+++ Day_02/data-types.js
@@ -0,0 +1,20 @@
+// let is a constant data type
+let word = "JavaScript"
+word[0] = 'Y'
+console.log(word)
+
+// is numbers equal ?
+let numOne = 3
+let numTwo = 3
+console.log(numOne == numTwo)
+
+// is strings equal ?
+let JS = 'Javascript'
+let PY = "Python"
+console.log(JS == PY)
+
+// is boolean equal ?
+let lightOn = true
+let lightOff = false
+console.log(lightOff == lightOn)
+
