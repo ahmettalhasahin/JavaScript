06d0769849594f031c502581c5d93c7cf3fd3a2a
Ahmet talha Sahin <ahmttlhshn@gmail.com>
2022-08-18 00:51:16 +0300

    Level 2 and level 3 of the exercises have been completed.

--- Day_02/data-types.js
+++ Day_02/data-types.js
@@ -109,3 +109,93 @@ console.log(examp.concat(' JavaScript'))
 console.log(challange.repeat(2))
 
 //Level 2
+
+/* 1.Using console.log() print out the following statement:
+The quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another. */
+console.log("\nThe quote 'There is no exercise better for the heart than reaching down and lifting people up.' by John Holmes teaches us to help one another.")
+
+/* 2.Using console.log() print out the following quote by Mother Teresa:
+"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead." */
+console.log("\n\"Love is not patronizing and charity isn't about pity, it is about love. Charity and love are the same -- with charity you give love, so don't just give money but reach out your hand instead.\"")
+
+//3.Check if typeof '10' is exactly equal to 10. If not make it exactly equal.
+console.log("\n")
+console.log('10' === 10)
+console.log(parseInt('10') === 10)
+
+//4.Check if parseFloat('9.8') is equal to 10 if not make it exactly equal with 10.
+console.log("\n")
+let num = Math.round('9.8')
+console.log(num == 10)
+
+//5.Check if 'on' is found in both python and jargon
+console.log("\n")
+console.log("phyton".includes("on") && "jargon".includes("on"))
+
+//6.I hope this course is not full of jargon. Check if jargon is in the sentence.
+console.log("\n")
+let jargon = "I hope this course is not full of jargon."
+console.log(jargon.includes("jargon"))
+
+//7.Generate a random number between 0 and 100 inclusively.
+console.log("\n")
+console.log((Math.random())*100)
+
+//8.Generate a random number between 50 and 100 inclusively.
+console.log("\n")
+console.log(((Math.random())*50) + 50)
+
+//9.Generate a random number between 0 and 255 inclusively.
+console.log("\n")
+console.log(Math.random() * 255)
+
+//10.Access the 'JavaScript' string characters using a random number.
+console.log("\n")
+let index = Math.floor(Math.random() * 10)
+let str = "JavaSCript"
+console.log(str[index])
+
+/*11.Use console.log() and escape characters to print the following pattern.
+1 1 1 1 1
+2 1 2 4 8
+3 1 3 9 27
+4 1 4 16 64
+5 1 5 25 125 */
+console.log("\n1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125")
+
+//12.Use substr to slice out the phrase because because because from the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log("\n")
+let slice = 'You cannot end a sentence with because because because is a conjunction'
+console.log(slice.indexOf("because"))
+let first = slice.substr(0,31)
+console.log(slice.lastIndexOf("because"))
+console.log("because".length)
+console.log(slice.length)
+let second = slice.substr(slice.lastIndexOf("because") + ("because".length) + 1)
+console.log(first.concat(second))
+//Level 3
+
+//1.'Love is the best thing in this world. Some found their love and some are still looking for their love.' Count the number of word love in this sentence.
+let sentence = 'Love is the best thing in this world. Some found their love and some are still looking for their love.'
+console.log("\n"+sentence)
+let love = sentence.match(/love/gi)
+console.log(love.length)
+
+//2.Use match() to count the number of all because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+console.log("\n")
+let count = 'You cannot end a sentence with because because because is a conjunction'
+console.log(count.match(/because/gi).length)
+
+/*3.Clean the following text and find the most frequent word (hint, use replace and regular expressions).
+    const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching' */
+console.log("\n")
+let cleaner = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching'
+console.log(cleaner.replace(/\%|\$|\@|\&|\#|\;/gi,''))
+
+//4.Calculate the total annual income of the person by extracting the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+console.log("\n")
+let array = 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
+let counter = array.match(/\d+/g)
+console.log(counter)
+let salary = (counter[0] * 12) + parseInt(counter[1]) + (counter[2] * 12)
+console.log(salary)
